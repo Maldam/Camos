a64aefadd1de7b78de5a020d0bbc2ab683672951
Maldam <damien.malice@hotmail.com>
2019-12-26 21:44:49 +0100

    Fonction de connexion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +13,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 export class AppComponent {
   constructor(
+    private router: Router,
+    public afAuth: AngularFireAuth,
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
@@ -20,6 +24,15 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
+      this.afAuth.authState.subscribe(auth => {
+        if (!auth) {
+          console.log('non connecté');
+          this.router.navigateByUrl('/connexion');
+        } else {
+          this.router.navigateByUrl('/');
+          console.log('Connecté: ' + auth.uid);
+        }
+      });
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
--- src/app/connexion/connexion.page.ts
+++ src/app/connexion/connexion.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-connexion',
@@ -6,8 +8,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./connexion.page.scss'],
 })
 export class ConnexionPage implements OnInit {
+  
+  loginData = {
+    email: '',
+    password: ''
+  };
 
-  constructor() { }
+  constructor(
+    public toastController: ToastController,
+    public afAuth: AngularFireAuth
+  ) { }
+
+  login() {
+  this.afAuth.auth.signInWithEmailAndPassword(this.loginData.email, this.loginData.password)
+  .then(auth => {
+    console.log('utilisateur connecté');
+  })
+  .catch(err => {
+    console.log('Erreur: ' + err);
+    this.errorMail();
+  });
+}
+
+async errorMail() {
+  const toast = await this.toastController.create({
+    message: 'Email ou mot de passe incorrect',
+    duration: 2000,
+    position: 'top'
+  });
+  toast.present();
+}
+
+signUp() {
+  this.afAuth.auth.createUserWithEmailAndPassword(this.loginData.email, this.loginData.password)
+  .then(auth => {
+    console.log('utilisateur connecté');
+  })
+  .catch(err => {
+    console.log('Erreur: ' + err);
+    this.errorMail();
+  });
+}
+
+
 
   ngOnInit() {
   }
