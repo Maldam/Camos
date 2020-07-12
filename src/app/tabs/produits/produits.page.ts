90e42d777986826ec71979763a90d21570143d98
Maldam <damien.malice@hotmail.com>
2020-07-12 22:15:03 +0200

    Barre de recherche produits

--- src/app/tabs/produits/produits.page.ts
+++ src/app/tabs/produits/produits.page.ts
@@ -9,7 +9,10 @@ import { ProduitModele } from '../../modeles/produit.modele';
   styleUrls: ['./produits.page.scss'],
 })
 export class ProduitsPage implements OnInit {
+
   public produits: Array<ProduitModele> = new Array<ProduitModele>();
+  public listeProduits: Array<ProduitModele> = new Array<ProduitModele>();
+  
   constructor(
     public produitsService: ProduitsService,
     public route: Router
@@ -18,12 +21,29 @@ export class ProduitsPage implements OnInit {
   // public imageDefaut(event: any) {
   //   event.target.src = 'https://upload.wikimedia.org/wikipedia/commons/e/e6/Pas_d%27image_disponible.svg';
   // }
+  
+  public rechercheProduit(ev: any){    
+    this.produits = this.listeProduits
+    
+    const val = ev.target.value;
+    console.log(val);
+    console.log(this.produits)
+    if (val && val.trim() !== ''){
+      this.produits = this.produits.filter((item: any) => {
+        return (item.nom.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+      
+      console.log(this.produits)
+    }
+  }
+
   public versVueProduit(produit: ProduitModele) {
     this.route.navigate(["afficher-produit"], { state: { data: produit } });
   }
   public ngOnInit() {
     this.produitsService.getProduits().subscribe(produits => {
       this.produits = produits;
+      this.listeProduits = this.produits;
     });
   }
 }
