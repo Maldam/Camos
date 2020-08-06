1b5ea63678f91a589f0d65b05f96254a5b6fe215
Maldam <damien.malice@hotmail.com>
2020-08-06 21:56:14 +0200

    ! invertion commit pecedent avant image default

--- src/app/afficher-produit/afficher-produit.page.ts
+++ src/app/afficher-produit/afficher-produit.page.ts
@@ -4,7 +4,6 @@ import { NavController, AlertController, LoadingController } from '@ionic/angula
 import { ProduitModele } from '../modeles/produit.modele';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormGroup, FormBuilder } from "@angular/forms";
-
 @Component({
   selector: 'app-afficher-produit',
   templateUrl: './afficher-produit.page.html',
@@ -14,7 +13,6 @@ export class AfficherProduitPage implements OnInit {
   public estChange: boolean = false;
   public imageChange: boolean = false;
   public nomChange: boolean = false;
-  public imageProduit: string;
   public form: FormGroup;
   public produit: ProduitModele = new ProduitModele();
   public image: string;
@@ -26,18 +24,13 @@ export class AfficherProduitPage implements OnInit {
     private formBuilder: FormBuilder,
     public loadingController: LoadingController,
     public alertController: AlertController,
-
   ) {
     this.activatedRoute.queryParams.subscribe(() => {
       if (this.router.getCurrentNavigation().extras.state) {
         this.produit = this.router.getCurrentNavigation().extras.state.data;
       }
     });
-    this.image = this.produit.imageURL
-    //this.imageOrigine = this.produit.imageURL
-    //this.imageProduit = this.imageOrigine
-    console.log("3")
-    console.log(this.imageOrigine)
+    //this.image = this.produit.imageURL
   }
   public async RemoveProduit(produit: ProduitModele) {
     if (confirm("Êtes-vous sûr de vouloir supprimer " + produit.nom + "?")) {
@@ -45,9 +38,7 @@ export class AfficherProduitPage implements OnInit {
       this.navCtrl.back()
     }
   }
-
   public async UpdateProduit(produit: ProduitModele, errorMessage: string) {
-
     const articleExiste = await this.alertController.create({
       header: 'Attention',
       message: 'Cet article existe déjà',
@@ -65,14 +56,15 @@ export class AfficherProduitPage implements OnInit {
           this.produit.nom = this.form.value.nomForm;
           this.produit.quantite = this.form.value.quantiteForm;
           this.produit.prix = this.form.value.prixForm;
-          this.produit.imageURL = 'https://firebasestorage.googleapis.com/v0/b/camos-266e6.appspot.com/o/Produits%2F' + this.produit.nom + '.jpg?alt=media&token=03dbf0d3-b9d6-40ae-99c7-2af2486a69e5'
-          this.nouvelleImage();
+          if (this.imageChange) {
+            this.nouvelleImage();
+            this.produit.imageURL = 'https://firebasestorage.googleapis.com/v0/b/camos-266e6.appspot.com/o/Produits%2F' + this.produit.nom + '.jpg?alt=media&token=03dbf0d3-b9d6-40ae-99c7-2af2486a69e5'
+          }
           this.produitsService.updateProduit(produit).then(ref => { this.navCtrl.back() });
           this.estChange = false;
           this.nomChange = false;
           this.imageChange = false;
-          //this.navCtrl.back();
-        } 
+        }
       } else {
         this.imageChange = false;
         await articleExiste.present();
@@ -85,25 +77,16 @@ export class AfficherProduitPage implements OnInit {
       }
     }
   }
-
   public async nouvelleImage() {
     const loading = await this.loadingController.create({
     });
-    if (this.imageChange) {
-      await loading.present();
-      console.log("1")
-      console.log(this.imageOrigine)
-      this.produitsService.deleteImage(this.imageOrigine)
-      var nomImage = 'Produits/' + this.produit.nom + '.jpg'
-      this.produitsService.ajouterImage(nomImage, this.image).then(ref => {
-        loading.dismiss();
-      })
-      this.image = 'https://firebasestorage.googleapis.com/v0/b/camos-266e6.appspot.com/o/Produits%2F' + this.produit.nom + '.jpg?alt=media&token=03dbf0d3-b9d6-40ae-99c7-2af2486a69e5'
-      //this.produitsService.produit.imageURL = this.produit.imageURL
-      this.imageChange = false;
-    }
+    await loading.present();
+    this.produitsService.deleteImage(this.imageOrigine)
+    var nomImage = 'Produits/' + this.produit.nom + '.jpg'
+    this.produitsService.ajouterImage(nomImage, this.image).then(ref => { loading.dismiss() })
+    this.image = 'https://firebasestorage.googleapis.com/v0/b/camos-266e6.appspot.com/o/Produits%2F' + this.produit.nom + '.jpg?alt=media&token=03dbf0d3-b9d6-40ae-99c7-2af2486a69e5'
+    this.imageChange = false;
   }
-
   public async changerPhoto(source: string) {
     if (source == 'galerie') {
       const galerieImage = await this.produitsService.openLibrary();
@@ -113,10 +96,8 @@ export class AfficherProduitPage implements OnInit {
       this.image = 'data:image/jpg;base64,' + cameraImage;
     }
   }
-
   public ngOnInit() {
-    console.log("2")
-    console.log(this.imageOrigine)
+    this.image = this.produit.imageURL
     this.imageOrigine = this.produit.imageURL
     this.form = this.formBuilder.group({
       nomForm: [this.produit.nom],
