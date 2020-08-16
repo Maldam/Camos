e789e37b3ad5dfd1037765446773c7bd6cb3cfbb
Maldam <damien.malice@hotmail.com>
2020-08-16 22:32:43 +0200

    Mise en place code commande ts

--- src/app/tabs/commandes/commandes.page.ts
+++ src/app/tabs/commandes/commandes.page.ts
@@ -11,6 +11,12 @@ export class CommandesPage implements OnInit {
   public userId: string;
   public mail: string;
   public method: any;
+  public commandesService: CommandesService,
+    public route: Router,
+    public network: Network,
+    public dialogs: Dialogs,
+  public commandes: Array<CommandeModele> = new Array<CommandeModele>();
+  public listeCommandes: Array<CommandeModele> = new Array<CommandeModele>();
   constructor(
     public afAuth: AngularFireAuth,
     public connexionService: ConnexionService
@@ -24,10 +30,43 @@ export class CommandesPage implements OnInit {
         this.method = auth.providerData[0].providerId;
       }
     });
+    
+    this.network.onDisconnect().subscribe(()=>
+    {
+      this.dialogs.alert("pas de connexion à internet")
+    });
+    this.network.onConnect().subscribe(()=>
+    {
+      setTimeout(()=>{
+      },2000);
+    });  
+
+
+
+
+  }
+
+
+
+  public rechercheCommande(ev: any){    
+    this.commandes = this.listeCommandes
+    const val = ev.target.value;
+    if (val && val.trim() !== ''){
+      this.commandes = this.commandes.filter((commande: any) => {
+        return (commande.nom.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+  public versVueCommande(commande: CommandeModele) {
+    this.route.navigate(["afficher-commande"], { state: { data: commande } });
   }
   public deconnexion() {
     this.connexionService.deconnexionUtilisateur();
   }
   public ngOnInit() {
+    this.commandesService.getCommandes().subscribe(commandes => {
+      this.commandes = commandes;
+      this.listeCommandes = this.commandes;
+    });
   }
 }
