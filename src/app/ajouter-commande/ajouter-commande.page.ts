05ff4fbc4e0124d8130e82548c8325fc84fd437c
Maldam <52838321+Maldam@users.noreply.github.com>
2020-08-17 14:30:04 +0200

    Addaptation code des pages ajout commande

--- src/app/ajouter-commande/ajouter-commande.page.ts
+++ src/app/ajouter-commande/ajouter-commande.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CommandeModele } from '../modeles/commande.modele';
+import { CommandesService } from '../services/commandes.service';
+import { LoadingController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-ajouter-commande',
@@ -6,10 +9,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ajouter-commande.page.scss'],
 })
 export class AjouterCommandePage implements OnInit {
+  public commande: CommandeModele = new CommandeModele();
+  public commandes: Array<CommandeModele> = new Array<CommandeModele>();
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private commandesService: CommandesService,
+    public loadingController: LoadingController,
+    public alertController: AlertController,
+  ) {
   }
-
+  public async ajoutCommande(commande: AjouterCommandePage) {
+    const loading = await this.loadingController.create({
+    });
+    const alert = await this.alertController.create({
+      header: 'Félicitation',
+      message: 'La commande a bien été ajouté',
+      buttons: ['OK']
+    });
+    const alertNom = await this.alertController.create({
+      header: 'Attention',
+      message: 'Nous avons besoin d\'un numéro de facture',
+      buttons: ['OK']
+    });
+    const articleExiste = await this.alertController.create({
+      header: 'Attention',
+      message: 'Cette commande existe déjà',
+      buttons: ['OK']
+    });
+    var index = this.commandesService.numeroIndex(this.commande.numero);
+    if (index === -1) {
+      if (this.commande.numero == undefined) {
+        await alertNom.present();
+      } else {
+        await loading.present();
+          await loading.dismiss();
+          await alert.present();
+        } 
+        this.commandesService.createCommande(this.commande).then(ref => { this.commande = new CommandeModele });
+    } else {
+      await articleExiste.present();
+    }
+  }
+  public ngOnInit() {
+  }
 }
