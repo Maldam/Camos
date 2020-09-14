8cec0f29aaf023f1500f3ae4294f99fbd6a6ba14
Maldam <52838321+Maldam@users.noreply.github.com>
2020-09-14 15:10:19 +0200

    Création de CoordoneesModele

---
+++ src/app/modeles/coordonnees.modele.ts
@@ -0,0 +1,19 @@
+export class CoordonneesModele {
+    public key: string;
+    public type: string = "";
+    public fonction: string = "";
+    public keyContact: string = "";
+    public pays: string = "";
+    public province: string = "";
+    public codePostal: number = 0;
+    public localite: string = "";
+    public rue: string = "";
+    public numero: number = 0;
+    public boite: string = "";
+    public numeroTelephone: string = "";
+    public numeroGSM: string = "";
+    public numeroFax: string = "";
+    public email: string = "";
+    constructor(
+    ) { }
+}
