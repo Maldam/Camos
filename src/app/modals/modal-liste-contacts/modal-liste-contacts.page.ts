71f05f401ba7412465c152ab21e9038aca0a07bd
Maldam <damien.malice@hotmail.com>
2020-09-16 00:28:12 +0200

    Mise en place de la modal liste-contact

--- src/app/modals/modal-liste-contacts/modal-liste-contacts.page.ts
+++ src/app/modals/modal-liste-contacts/modal-liste-contacts.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { ContactModele } from 'src/app/modeles/contact.modele';
+import { ContactsService } from 'src/app/services/contacts.service';
 import { ModalContactPage } from '../modal-contact/modal-contact.page';
-
 @Component({
   selector: 'app-modal-liste-contacts',
   templateUrl: './modal-liste-contacts.page.html',
@@ -9,12 +10,15 @@ import { ModalContactPage } from '../modal-contact/modal-contact.page';
 })
 export class ModalListeContactsPage implements OnInit {
   public keyEntreprise: string;
-
-  constructor(private modalController: ModalController) { }
-    public async contactModal(keyEntreprise: string){
+  public contacts: Array<ContactModele> = new Array<ContactModele>()
+  constructor(
+    private modalController: ModalController,
+    private contactsService: ContactsService,
+  ) { }
+  public async contactModal(keyEntreprise: string) {
     const modal = await this.modalController.create({
       component: ModalContactPage,
-      componentProps:{keyEntreprise}
+      componentProps: { keyEntreprise }
     });
     modal.onWillDismiss()
     //.then(dataReturned => {
@@ -22,8 +26,25 @@ export class ModalListeContactsPage implements OnInit {
     //})
     return await modal.present()
   }
-  ngOnInit() {
-    
+  public async versVueContact(contact: ContactModele) {
+    const modal = await this.modalController.create({
+      component: ModalContactPage,
+      componentProps: { contact }
+    });
+    modal.onWillDismiss()
+    //.then(dataReturned => {
+    //  this.client = dataReturned.data;
+    //})
+    return await modal.present()
   }
-
+  async closeModal() {
+    await this.modalController.dismiss();
+  }
+  ngOnInit() {
+    this.contactsService.getContactsClient(this.keyEntreprise).subscribe(contacts => {
+      this.contacts = contacts;
+      ///console.log(this.contacts)
+      //this.contacts.sort((a,b) => b.nom - a.nom);
+    })
+  }
 }
